b7acecdd2d86b82b48ec77667bd07a079deeb6e8
Yiliya <1006352171@qq.com>
2020-04-27 23:12:58 +0800

    添加新闻详情页(特效未完成)

---
+++ src/components/Journalism/Journalism.vue
@@ -0,0 +1,306 @@
+<!--  -->
+<template>
+  <div>
+    <div class="w" style="height:40px"></div>
+    <div class="main w" >
+      <div class="mainLeft">
+        <!-- 面包屑 -->
+        <nav class="bread mw">
+          <ul>
+            <li>
+              <a href="">首页</a>
+              <i class="el-icon-arrow-right"></i>
+            </li>
+          <li>
+              <a href="">五金资讯</a>
+              <i class="el-icon-arrow-right"></i>
+            </li><li>
+              <span>详情</span>
+            </li>
+            
+          </ul>
+        </nav>
+        <div class="mw" style="height:30px"></div>
+          <!-- 主体 -->
+        <h1 class="mainTitle">
+          汉匠万里挑一的选址 FAST为何选择这个"窝"里建
+        </h1>
+        <div class="mw" style="height:20px"></div>
+        <div>
+          <span class="mainDate">2019-10-21 15:05:20</span>
+        </div>
+        <div class="mw" style="height:30px"></div>
+        
+        <div class="mainText">
+          <p>世界最大单口径射电望远镜今天在贵州省平塘县正式投入使用，这一500米口径的球面射电望远镜建设在喀斯特地貌天然形成的洼地中。</p>
+          <p>世界最大单口径射电望远镜(FAST)今天在贵州省平塘县正式投入使用，这一500米口径的球面射电望远镜建设在喀斯特地貌天然形成的洼地中，被誉为“观天巨眼”。</p>
+          <p>由于FAST的落户，安放“观天巨眼”的洼地也成为科学界关注的焦点，人们称这个名叫“大窝凼”的自然洼地为“地球上最美的眼窝”。</p>
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+        </div>
+        <div class="mainImg">
+          <div>
+            <img src="http://v1.cdn-static.cn/2018/08/15/99d56c5bc8b8f8836d21850410ea7be4.jpeg" alt="">
+          </div>
+        </div>
+        <div class="mw" style="height:60px"></div>
+        <div class="mainBottom">
+          <a href="">
+            <p>上一篇&nbsp;&nbsp;:&nbsp;&nbsp; <span>上市公司炒房记 高溢价“卖楼”频现</span></p>
+          </a>
+          <a href="">
+            <p>
+              <span>
+                上市公司炒房记 高溢价“卖楼”频现
+              &nbsp;&nbsp; 
+              </span>:&nbsp;&nbsp;下一篇
+            </p>
+          </a>
+        </div>
+        
+      </div>
+      <!-- 主体左边结束 -->
+
+      <!-- 主体右边开始 -->
+      <div class="mainRight">
+        <h2>最新新闻</h2>
+      <ul class="rightMain">
+        <li>
+          <a href="">
+            <div class="right_Main_img">
+              <img src="http://s-14587.p.cdn-static.cn/10733_157139029584116.png" alt="">
+            </div>
+            <div class="right_Main_Text">
+              <h3 style="padding-top:19px;color:black">汉匠万里挑一的选址 FAST为何选择这个"窝"里建</h3>
+              <span style="padding-top:3px;">世界最大单口径射电望远镜今天在贵州省平塘县正式投入使用，这一500米口径的球面射电望远镜建设在喀斯特地貌天然形成的洼地中。</span>
+              <span  style="padding-top:20px;">2019-10-21 15:05:20</span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <div class="right_Main_img">
+              <img src="http://s-14587.p.cdn-static.cn/10733_157139029584116.png" alt="">
+            </div>
+            <div class="right_Main_Text">
+              <h3 style="padding-top:19px;color:black">汉匠万里挑一的选址 FAST为何选择这个"窝"里建</h3>
+              <span style="padding-top:3px;">世界最大单口径射电望远镜今天在贵州省平塘县正式投入使用，这一500米口径的球面射电望远镜建设在喀斯特地貌天然形成的洼地中。</span>
+              <span  style="padding-top:20px;">2019-10-21 15:05:20</span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <div class="right_Main_img">
+              <img src="http://s-14587.p.cdn-static.cn/10733_157139029584116.png" alt="">
+            </div>
+            <div class="right_Main_Text">
+              <h3 style="padding-top:19px;color:black">汉匠万里挑一的选址 FAST为何选择这个"窝"里建</h3>
+              <span style="padding-top:3px;">世界最大单口径射电望远镜今天在贵州省平塘县正式投入使用，这一500米口径的球面射电望远镜建设在喀斯特地貌天然形成的洼地中。</span>
+              <span  style="padding-top:20px;">2019-10-21 15:05:20</span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <div class="right_Main_img">
+              <img src="http://s-14587.p.cdn-static.cn/10733_157139029584116.png" alt="">
+            </div>
+            <div class="right_Main_Text">
+              <h3 style="padding-top:19px;color:black">汉匠万里挑一的选址 FAST为何选择这个"窝"里建</h3>
+              <span style="padding-top:3px;">世界最大单口径射电望远镜今天在贵州省平塘县正式投入使用，这一500米口径的球面射电望远镜建设在喀斯特地貌天然形成的洼地中。</span>
+              <span  style="padding-top:20px;">2019-10-21 15:05:20</span>
+            </div>
+          </a>
+        </li>
+        
+      </ul>
+      </div>
+
+      <!-- 主体右边结束 -->
+    </div>
+
+
+
+
+
+
+  </div>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+import headPicture from "@/components/headPicture/headPicture";
+import BottomModule from "@/components/BottomModule/BottomModule";
+
+export default {
+  name: "",
+  //import引入的组件需要注入到对象中才能使用
+  components: {
+    BottomModule,
+    headPicture
+  },
+  data() {
+    //这里存放数据
+    return {
+      dateilist: "" //列表数据
+    };
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {},
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {
+    //  console.log(this.$store.state.Journalism_id);
+
+    let api = "/News_detail.json";
+
+    this.axios.get(api).then(response => {
+      this.dateilist = response.data.data;
+      console.log(this.dateilist);
+    });
+  },
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+
+<style scoped>
+.w {
+  width: 1200px;
+  margin: 0 auto;
+}
+.mw {
+  width: 100%;
+}
+.main {
+  position: relative;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+}
+.mainLeft {
+  float: left;
+  width: 70%;
+  margin-bottom: 80px;
+}
+
+/* 面包屑 */
+.bread {
+  color: #606297;
+}
+.bread ul {
+}
+.bread li {
+  display: inline-block;
+  padding: 10px 8px;
+}
+.bread li a {
+}
+.bread a {
+  padding: 10px 0;
+}
+.bread a:hover,
+.bread span:hover {
+  color: #40b8ff;
+}
+.bread li i {
+  padding: 0 0 0 5px;
+}
+/* 主体文字 */
+.mainText {
+}
+.mainDate {
+  color: #b2aab2;
+}
+.mainText p {
+  width: 90%;
+  text-indent: 1.5rem;
+  margin: 15px 0;
+}
+/* 主体图片 */
+
+.mainImg {
+  /* width: 80%; */
+}
+.mainImg img {
+  width: 100%;
+}
+/* 主体底部 */
+.mainBottom {
+  display: flex;
+  justify-content: space-between;
+  border-top: 1px solid #b2aab2;
+  padding: 10px;
+}
+.mainBottom a {
+  padding: 15px 20px;
+}
+.mainBottom p {
+  display: inline-block;
+}
+/* 主体右边 */
+.mainRight {
+  float: right;
+  width: 30%;
+  height: 600px;
+  padding-top: 72px;
+}
+
+.mainRight h2 {
+  padding: 8px 0;
+  font-weight: 400;
+}
+.rightMain {
+  float: right;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  padding: 8px 0;
+
+}
+.right_Main_img {
+  width: 35%;
+  height: auto;
+}
+.rightMain li{
+  position: relative;
+  margin-top: 15px;
+}
+.rightMain li img {
+  float: left;
+  width: 100%;
+  height: 110px;
+}
+.right_Main_Text {
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  width: 65%;
+  color: #606297; 
+}
+
+.right_Main_Text h3,
+.right_Main_Text span {
+  float: left;
+  padding-left: 7px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,23 +7,25 @@ import AboutUs from '@/views/AboutUs.vue'    //关于我们
 import Projects from '@/views/Projects.vue'    //项目展示
 import News from '@/views/News.vue'    //新闻中心
 import ContactUs from '@/views/ContactUs.vue'    //联系我们
+import NewsDetails from '@/views/News_details_page.vue'    //新闻详情页
 
 Vue.use(Router)
 
 const routes = [
-	{ path: "/", component: AppBar ,redirect: '/AppBar/Home'},
-    { path: '/AppBar', component: AppBar ,redirect: '/AppBar/Home' ,
-			children:[
-				{path: 'Home', component: Home},
-				{path: 'AboutUs', component: AboutUs},
-				{path: 'Projects', component: Projects},
-				{path: 'News', component: News},
-				{path: 'ContactUs', component: ContactUs},
-			],
-	},
-    // { path: "/Main", component: Main },
-    
-  ];
+  { path: "/", component: AppBar, redirect: '/AppBar/Home' },
+  {
+    path: '/AppBar', component: AppBar, redirect: '/AppBar/Home',
+    children: [
+      { path: 'Home', component: Home },
+      { path: 'AboutUs', component: AboutUs },
+      { path: 'Projects', component: Projects },
+      {path: 'News', component: News  },
+      { path: 'ContactUs', component: ContactUs },
+
+    ],
+  },
+  { path: "/AppBar/News/NewsDetails", component: NewsDetails },
+];
 
 export default new Router({
   routes
