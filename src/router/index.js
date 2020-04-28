aef2122ea5f25e9bfae8a89b2d3f24e38393e6d9
1147872997 <1147872997@qq.com>
2020-04-28 08:55:34 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
-import AppBar from '@/views/AppBar.vue'     //导航栏
+import index from '@/views/index.vue'     //导航栏
 import Home from '@/views/Home.vue'       //首页
 import AboutUs from '@/views/AboutUs.vue'    //关于我们
 import Projects from '@/views/Projects.vue'    //项目展示
@@ -11,8 +11,8 @@ import ContactUs from '@/views/ContactUs.vue'    //联系我们
 Vue.use(Router)
 
 const routes = [
-	{ path: "/", component: AppBar ,redirect: '/AppBar/Home'},
-    { path: '/AppBar', component: AppBar ,redirect: '/AppBar/Home' ,
+	{ path: "/", component: index ,redirect: '/index/Home'},
+    { path: '/index', component: index ,redirect: '/index/Home' ,
 			children:[
 				{path: 'Home', component: Home},
 				{path: 'AboutUs', component: AboutUs},
