724f26e59efd3f8628907c37b3a35679893eea76
1147872997 <1147872997@qq.com>
2020-04-30 23:15:37 +0800

    修改首页组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,24 @@
 <!-- 首页 -->
 <template>
 <div class='Home'>
+  <!-- 轮播组件 -->
 <lunbo></lunbo>
+<!-- 工业界组件 -->
+<Industry></Industry>
 </div>
 </template>
 
 <script>
-import lunbo from '@/components/Home/lunbo'
+import lunbo from '@/components/Home/lunbo';
+import Industry from '@/components/Home/Industry';
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
 export default {
   name: 'Home',
 //import引入的组件需要注入到对象中才能使用
 components: {
-  lunbo
+  lunbo,
+  Industry
 },
 data() {
 //这里存放数据
